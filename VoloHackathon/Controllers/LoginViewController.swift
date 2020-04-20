df5c87d5c8d8e2cd5c1e09a5da1b6b90427b18c8
brendon cecilio <brendon.cecilio@gmail.com>
2020-04-20 18:45:10 -0400

    background color change

--- VoloHackathon/Controllers/LoginViewController.swift
+++ VoloHackathon/Controllers/LoginViewController.swift
@@ -12,7 +12,7 @@ class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.backgroundColor = #colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1)
     }
     
 
