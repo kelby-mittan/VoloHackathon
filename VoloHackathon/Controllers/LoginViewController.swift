e4ef52bfda2b0c67a9429da7b9430dc6cb75e916
brendon cecilio <brendon.cecilio@gmail.com>
2020-04-21 13:17:16 -0400

    authSession for accountState in loginVC

--- VoloHackathon/Controllers/LoginViewController.swift
+++ VoloHackathon/Controllers/LoginViewController.swift
@@ -20,8 +20,11 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var lookingButton: UIButton!
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var errorLabel: UILabel!
+    
     
     private var accountState: AccountState = .existingUser
+    private var authentication = AuthenticationSession()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -44,20 +47,66 @@ class LoginViewController: UIViewController {
         guard let username = usernameTextField.text, !username.isEmpty, let password = passwordTextField.text, !password.isEmpty else {
             return
         }
+        continueLoginFlow(email: username, password: password)
     }
     
     @IBAction func lookingButtonPressed(_ sender: UIButton) {
         guard let username = usernameTextField.text, !username.isEmpty, let password = passwordTextField.text, !password.isEmpty else {
             return
         }
+        continueLoginFlow(email: username, password: password)
     }
     
     private func navigateToUserView() {
-        
+        UIViewController.showViewController(storyBoardName: "Volunteer", viewControllerId: "VolunteerTabBarController")
     }
     
     private func navigateToPostView() {
         
+    }
+    
+    private func continueLoginFlow(email: String, password: String) {
+        
+        if accountState == .existingUser {
+            authentication.signExistingUser(email: email, password: password) { [weak self] (result) in
+                switch result {
+                case .failure(let error):
+                    DispatchQueue.main.async {
+                        self?.errorLabel.text = "\(error)"
+                        self?.errorLabel.textColor = .systemRed
+                    }
+                case .success(_):
+                    self?.navigateToUserView()
+                }
+            }
+        } else {
+            authentication.createNewUser(email: email, password: password) { [weak self] (result) in
+                switch result {
+                case .failure(let error):
+                    DispatchQueue.main.async {
+                        self?.errorLabel.text = "\(error)"
+                        self?.errorLabel.textColor = .systemRed
+                    }
+                case .success(let authDataResult):
+                    DispatchQueue.main.async {
+                        self?.createDatabaseUser(authDataResult: authDataResult)
+                    }
+                }
+            }
+        }
+    }
+    
+    private func createDatabaseUser(authDataResult: AuthDataResult) {
+        DatabaseService.shared.createDatabaseUser(authDataResult: authDataResult) { [weak self] (result) in
+            switch result {
+            case .failure(let error):
+                DispatchQueue.main.async {
+                    print("\(error)")
+                }
+            case .success(_):
+                self?.navigateToUserView()
+            }
+        }
     }
     
 }
