c83ef1a5120bfd63f585fec9b7ff813641a89d7b
brendon cecilio <brendon.cecilio@gmail.com>
2020-04-21 04:30:04 -0400

    added elements to loginStoryboard

--- VoloHackathon/Controllers/LoginViewController.swift
+++ VoloHackathon/Controllers/LoginViewController.swift
@@ -9,11 +9,30 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    @IBOutlet weak var volunteerButton: UIButton!
+    @IBOutlet weak var lookingButton: UIButton!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = #colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1)
     }
     
 
+    private func configureButtonUI() {
+        
+        volunteerButton.layer.cornerRadius = 60
+        volunteerButton.layer.borderWidth = 1.0
+        lookingButton.layer.cornerRadius = 60
+        lookingButton.layer.borderWidth = 1.0
+    }
+    
+    @IBAction func volunterrButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func looingButtonPressed(_ sender: UIButton) {
+    }
+    
 }
