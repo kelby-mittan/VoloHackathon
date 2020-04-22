f37591e002890d4e869b7fbea8de5c7a40d11938
brendon cecilio <brendon.cecilio@gmail.com>
2020-04-21 21:49:25 -0400

    added custom segue class + added base code for signUpVC

--- VoloHackathon/Controllers/LoginViewController.swift
+++ VoloHackathon/Controllers/LoginViewController.swift
@@ -16,12 +16,9 @@ enum AccountState {
 
 class LoginViewController: UIViewController {
     
-    @IBOutlet weak var volunteerButton: UIButton!
-    @IBOutlet weak var lookingButton: UIButton!
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var errorLabel: UILabel!
-    
     
     private var accountState: AccountState = .existingUser
     private var authentication = AuthenticationSession()
@@ -29,36 +26,16 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = #colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1)
-        configureButtonUI()
     }
     
-
-    private func configureButtonUI() {
-        
-//        volunteerButton.layer.cornerRadius = 10
-//        volunteerButton.layer.borderWidth = 0.8
-//        volunteerButton.layer.borderColor = UIColor.white.cgColor
-//        lookingButton.layer.cornerRadius = 10
-//        lookingButton.layer.borderWidth = 0.8
-//        lookingButton.layer.borderColor = UIColor.white.cgColor
+    @IBAction func signInButtonPresser() {
+        guard let username = usernameTextField.text, !username.isEmpty, let password = passwordTextField.text, !password.isEmpty else {
+                    errorLabel.text = "Both textfields must be filled!"
+                    errorLabel.textColor = .systemRed
+                    return
+                }
+        continueLoginFlow(email: username, password: password)
     }
-    
-//    @IBAction func volunterrButtonPressed(_ sender: UIButton) {
-//        guard let username = usernameTextField.text, !username.isEmpty, let password = passwordTextField.text, !password.isEmpty else {
-//            errorLabel.text = "Both textfields must be filled!"
-//            errorLabel.textColor = .systemRed
-//            return
-//        }
-//        continueLoginFlow(email: username, password: password)
-//    }
-    
-//    @IBAction func lookingButtonPressed(_ sender: UIButton) {
-//        guard let username = usernameTextField.text, !username.isEmpty, let password = passwordTextField.text, !password.isEmpty else {
-//            errorLabel.text = "Both fields must be filled"
-//            return
-//        }
-//        continueLoginFlow(email: username, password: password)
-//    }
     
     private func navigateToUserView() {
         UIViewController.showViewController(storyBoardName: "Volunteer", viewControllerId: "VolunteerTabBarController")
@@ -79,32 +56,7 @@ class LoginViewController: UIViewController {
                 }
             }
         } else {
-            authentication.createNewUser(email: email, password: password) { [weak self] (result) in
-                switch result {
-                case .failure(let error):
-                    DispatchQueue.main.async {
-                        self?.errorLabel.text = "\(error)"
-                        self?.errorLabel.textColor = .systemRed
-                    }
-                case .success(let authDataResult):
-                    DispatchQueue.main.async {
-                        self?.createDatabaseUser(authDataResult: authDataResult)
-                    }
-                }
-            }
-        }
-    }
-    
-    private func createDatabaseUser(authDataResult: AuthDataResult) {
-        DatabaseService.shared.createDatabaseUser(authDataResult: authDataResult) { [weak self] (result) in
-            switch result {
-            case .failure(let error):
-                DispatchQueue.main.async {
-                    print("\(error)")
-                }
-            case .success(_):
-                self?.navigateToUserView()
-            }
+            errorLabel.text = "User does not exist. Create a new user or enter valid information."
         }
     }
 }
--- VoloHackathon/Storyboards/SignUpViewController.swift
+++ VoloHackathon/Storyboards/SignUpViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class SignUpViewController: UIViewController {
     
@@ -14,11 +15,15 @@ class SignUpViewController: UIViewController {
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var volunteerButton: UIButton!
     @IBOutlet weak var postButton: UIButton!
+    @IBOutlet weak var errorLabel: UILabel!
     
+    public var userType = ["Volunteer","Poster"]
+    private var accountState: AccountState = .newUser
+    private var authentication = AuthenticationSession()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        configureButtons()
     }
     
     private func configureButtons() {
@@ -34,6 +39,54 @@ class SignUpViewController: UIViewController {
     }
     
     @IBAction func signUpButtonPressed(_ sender: UIButton) {
+        guard let username = usernameTextField.text, !username.isEmpty, let password = passwordTextField.text, !password.isEmpty else {
+            errorLabel.text = "Both textfields must be filled"
+            errorLabel.textColor = .systemRed
+            return
+        }
+    }
+    
+    private func continueSignUpFlow(username: String, password: String) {
+        if accountState == .newUser {
+            authentication.createNewUser(email: username, password: password) { [weak self] (result) in
+                switch result {
+                case .failure(let error):
+                    DispatchQueue.main.async {
+                        self?.errorLabel.text = "\(error)"
+                        self?.errorLabel.textColor = .systemRed
+                    }
+                case .success(let authDataResult):
+                    DispatchQueue.main.async {
+                        self?.createDatabaseUser(authDataResult: authDataResult, userType: self?.userType.first ?? "no userType")
+//                        self?.naviga
+                    }
+                }
+            }
+        }
+    }
+    
+    private func navigateToVolunteerView() {
+        UIViewController.showViewController(storyBoardName: "Volunteer", viewControllerId: "VolunteerTabBarController")
+    }
+    
+    private func navigateToPostView() {
+        
+    }
+    
+    private func createDatabaseUser(authDataResult: AuthDataResult, userType: String) {
+        DatabaseService.shared.createDatabaseUser(authDataResult: authDataResult, userType: userType) { [weak self] (result) in
+            switch result {
+            case .failure(let error):
+                DispatchQueue.main.async {
+                    self?.errorLabel.text = "\(error)"
+                    self?.errorLabel.textColor = .systemRed
+                }
+            case .success:
+                DispatchQueue.main.async {
+                    self?.navigateToVolunteerView()
+                }
+            }
+        }
     }
     
 }
