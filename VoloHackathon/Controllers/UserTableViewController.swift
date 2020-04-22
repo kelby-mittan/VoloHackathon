1726198f18a7a5a6c72b4d51daaf2799abf1129d
brendon cecilio <brendon.cecilio@gmail.com>
2020-04-22 15:10:15 -0400

    tableView code added

--- VoloHackathon/Controllers/UserTableViewController.swift
+++ VoloHackathon/Controllers/UserTableViewController.swift
@@ -9,8 +9,24 @@
 import UIKit
 
 class UserTableViewController: UIViewController {
+    
+    private let tableView = UserTableView()
+    
+    override func loadView() {
+        view = tableView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+}
+
+extension UserTableViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
     }
 }
--- VoloHackathon/Views/UserTableView.swift
+++ VoloHackathon/Views/UserTableView.swift
@@ -9,6 +9,34 @@
 import UIKit
 
 class UserTableView: UIView {
-
     
+    private lazy var tableView: UITableView = {
+        let table = UITableView()
+        return table
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: UIScreen.main.bounds)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        setupTableView()
+    }
+    
+    private func setupTableView() {
+        addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
 }
