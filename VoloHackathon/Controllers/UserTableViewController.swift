0a9ef3e3320abc8e54914282e4f0f65462957072
brendon cecilio <brendon.cecilio@gmail.com>
2020-04-22 15:04:01 -0400

    added VC and View file

---
+++ VoloHackathon/Controllers/UserTableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  UserTableViewController.swift
+//  VoloHackathon
+//
+//  Created by Brendon Cecilio on 4/22/20.
+//  Copyright © 2020 Kelby Mittan. All rights reserved.
+//
+
+import UIKit
+
+class UserTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ VoloHackathon/Views/UserTableView.swift
@@ -0,0 +1,14 @@
+//
+//  UserTableView.swift
+//  VoloHackathon
+//
+//  Created by Brendon Cecilio on 4/22/20.
+//  Copyright © 2020 Kelby Mittan. All rights reserved.
+//
+
+import UIKit
+
+class UserTableView: UIView {
+
+    
+}
