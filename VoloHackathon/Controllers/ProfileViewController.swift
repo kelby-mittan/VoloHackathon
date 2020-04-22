09f12da003ccc5431f39f282db6711fbffd87f1e
Ameni Alsaydi <amenialsaydi@pursuit.org>
2020-04-22 18:09:57 -0400

    created a profile view controller

---
+++ VoloHackathon/Controllers/ProfileViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ProfileViewController.swift
+//  VoloHackathon
+//
+//  Created by Amy Alsaydi on 4/22/20.
+//  Copyright © 2020 Kelby Mittan. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    private var profileView = ProfileView()
+    
+    override func loadView() {
+        view = profileView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    
+
+}
