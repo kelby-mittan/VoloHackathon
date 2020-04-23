7bce9d70cc950860e014347f6f9bd71118cdaa9e
Ameni Alsaydi <amenialsaydi@pursuit.org>
2020-04-22 23:23:42 -0400

    added function to convert city to cordinates

--- VoloHackathon/Controllers/PostDetailController.swift
+++ VoloHackathon/Controllers/PostDetailController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import Kingfisher
 import FirebaseAuth
+import CoreLocation
 
 class PostDetailController: UIViewController {
     
@@ -112,6 +113,24 @@ class PostDetailController: UIViewController {
         detailView.largeLabel.text = post.shortDescription
         detailView.smallLabel2.text = post.description
         
+        let city = post.location
+        getCoordinateFrom(address: city) { coordinate, error in
+            guard let coordinate = coordinate, error == nil else {
+                print("no city")
+                return
+                
+            }
+            // don't forget to update the UI from the main thread
+            DispatchQueue.main.async {
+                print("Location:", coordinate) // Rio de Janeiro, Brazil Location: CLLocationCoordinate2D(latitude: -22.9108638, longitude: -43.2045436)
+            }
+
+        }
+        
+    }
+    
+    private func getCoordinateFrom(address: String, completion: @escaping(_ coordinate: CLLocationCoordinate2D?, _ error: Error?) -> () ) {
+        CLGeocoder().geocodeAddressString(address) { completion($0?.first?.location?.coordinate, $1) }
     }
     
 }
