37d6c003c569075d9536ec3e2660df16ee5d07a6
Kelby Mittan <kelbymittan@pursuit.org>
2020-04-28 17:22:40 -0400

    working on readme

---
+++ VoloHackathon/Controllers/VolunteerTabBarController 2.swift
@@ -0,0 +1,22 @@
+//
+//  VolunteerTabBarController.swift
+//  VoloHackathon
+//
+//  Created by Amy Alsaydi on 4/20/20.
+//  Copyright © 2020 Kelby Mittan. All rights reserved.
+//
+
+import UIKit
+
+class VolunteerTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+
+}
---
+++ VoloHackathon/Extensions/UITextField+Extension 2.swift
@@ -0,0 +1,32 @@
+//
+//  UITextField+Extension.swift
+//  VoloHackathon
+//
+//  Created by Kelby Mittan on 4/21/20.
+//  Copyright © 2020 Kelby Mittan. All rights reserved.
+//
+
+import UIKit
+extension UITextField {
+    
+    func setInputViewDatePicker(target: Any, selector: Selector) {
+        // Create a UIDatePicker object and assign to inputView
+        let screenWidth = UIScreen.main.bounds.width
+        let datePicker = UIDatePicker(frame: CGRect(x: 0, y: 0, width: screenWidth, height: 216))//1
+        datePicker.datePickerMode = .dateAndTime //2
+        self.inputView = datePicker //3
+        
+        // Create a toolbar and assign it to inputAccessoryView
+        let toolBar = UIToolbar(frame: CGRect(x: 0.0, y: 0.0, width: screenWidth, height: 44.0)) //4
+        let flexible = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil) //5
+        let cancel = UIBarButtonItem(title: "Cancel", style: .plain, target: nil, action: #selector(tapCancel)) // 6
+        let barButton = UIBarButtonItem(title: "Done", style: .plain, target: target, action: selector) //7
+        toolBar.setItems([cancel, flexible, barButton], animated: false) //8
+        self.inputAccessoryView = toolBar //9
+    }
+    
+    @objc func tapCancel() {
+        self.resignFirstResponder()
+    }
+    
+}
